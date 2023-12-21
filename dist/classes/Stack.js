8175383a44fe1016849a4c0564e1800e8c3696a8
Camilo Parra <caparra92@gmail.com>
2023-12-21 18:47:47 -0500

    Stack class exported in index.ts

--- dist/classes/Stack.js
+++ dist/classes/Stack.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Stack = void 0;
 /**
  * Stack implementation class with basic operations.
  * @public
@@ -10,13 +11,17 @@ var Stack = /** @class */ (function () {
         this.count = 0;
     }
     /**
-    *
+    * The push() method inserts a new item in the stack and increments its counter size.
     * @param element
     */
     Stack.prototype.push = function (element) {
         this.items[this.count] = element;
         this.count++;
     };
+    /**
+    * The pop() method removes the last item in the stack and returns the element.
+    * @return {any}
+    */
     Stack.prototype.pop = function () {
         if (this.isEmpty()) {
             return undefined;
@@ -26,15 +31,31 @@ var Stack = /** @class */ (function () {
         delete this.items[this.count];
         return result;
     };
+    /**
+    * The peek() method returns the last item in the stack.
+    * @return {any}
+    */
     Stack.prototype.peek = function () {
         return this.items[this.count - 1];
     };
+    /**
+    * The size() method returns the stack's lenght.
+    * @return {number}
+    */
     Stack.prototype.size = function () {
         return this.count;
     };
+    /**
+    * The isEmpty() method checks if the stack is empty or not.
+    * @return {boolean}
+    */
     Stack.prototype.isEmpty = function () {
         return this.count === 0;
     };
+    /**
+    * The clear() method removes all the items in the stack and reset the size counter.
+    * @return {void}
+    */
     Stack.prototype.clear = function () {
         this.items = {};
         this.count = 0;
@@ -55,4 +76,4 @@ var Stack = /** @class */ (function () {
     };
     return Stack;
 }());
-exports.default = Stack;
+exports.Stack = Stack;
