33ebc9172bf9b4c49b6518685a06b54898bca147
Camilo Parra <caparra92@gmail.com>
2023-12-26 22:51:14 -0500

    utils and models exported

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Deque = exports.Queue = exports.Stack = void 0;
+exports.LinkedList = exports.Deque = exports.Queue = exports.Stack = void 0;
 var Stack_1 = require("./classes/Stack");
 Object.defineProperty(exports, "Stack", { enumerable: true, get: function () { return Stack_1.Stack; } });
 var Queue_1 = require("./classes/Queue");
 Object.defineProperty(exports, "Queue", { enumerable: true, get: function () { return Queue_1.Queue; } });
 var Deque_1 = require("./classes/Deque");
 Object.defineProperty(exports, "Deque", { enumerable: true, get: function () { return Deque_1.Deque; } });
+var LinkedList_1 = require("./classes/LinkedList");
+Object.defineProperty(exports, "LinkedList", { enumerable: true, get: function () { return LinkedList_1.LinkedList; } });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export { defaultEquals } from './util/defaultEquals'
+export { Node, DoublyNode } from './models/linked-list-models'
+
 export { Stack } from './classes/Stack'
 export { Queue } from './classes/Queue'
 export { Deque } from './classes/Deque'
